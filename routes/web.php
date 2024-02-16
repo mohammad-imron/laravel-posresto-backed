1ce7d575ac1d848de44b31107da4af05ada34905
mohammad-imron <ronnie_plsr@yahoo.com>
2024-02-17 00:05:10 +0700

    add product

--- routes/web.php
+++ routes/web.php
@@ -27,20 +27,3 @@ Route::middleware(['auth'])->group(function(){
     Route::resource('users',userController::class);
     Route::resource('products',ProductController::class);
 });
-
-Route::get('storage/{filename}', function ($filename)
-{
-    $path = storage_path('public/' . $filename);
-
-    if (!File::exists($path)) {
-        abort(404);
-    }
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $response = Response::make($file, 200);
-    $response->header("Content-Type", $type);
-
-    return $response;
-});
